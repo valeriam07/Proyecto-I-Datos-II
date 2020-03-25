28d1251df27a0705c0642b8029a5c8fced582e99
stphn1117 <stephanie1117@hotmail.es>
2020-03-24 20:21:28 -0600

    Prueba de biblioteca

---
+++ Prueba1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "LibreriaPrueba.h"
+
+int suma(int a, int b){
+ return a+b;
+
+}
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include"LibreriaPrueba.h"
+
+using namespace std;
+/*
+para compilar el codigo, abrir la terminal y escribir
+
+-- g++ -Wall main.cpp Prueba1.cpp -o salida
+
+//lo cual genera un archivo ejecutable llamado salida
+
+--./salida
+
+//lo cual ejecuta el programa
+
+*/
+int main(){
+	int resultado = suma(1,1);
+	cout << "El resultado es: " << resultado << "\n";
+	return 0;
+
+}
