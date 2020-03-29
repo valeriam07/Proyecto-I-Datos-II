e0b229e430442b11bfad0ee860b642101af5d86a
stphn1117 <stephanie1117@hotmail.es>
2020-03-28 21:14:53 -0600

    "ajustes en el servidor para que su actualización sea automática"

--- PTypescript/app.ts
+++ PTypescript/app.ts
@@ -3,10 +3,17 @@ import http from 'http';
 http
 
     .createServer((request,response) => {
-    response.end('Hello, type');
+    response.end('Hello, world');
     })
-    .listen(3000, () => console.log("Server started."))
+    .listen(3000, () => console.log("Server stopped."))
 
     //shift +ctrl+b 
 
 
+/* crear una terminal para iniciar el server luego iniciar otra para establecer
+ una conexion desde otra terminal usando "http: 3000"*/
+
+ /* primero crear una ventana de tareas (watch), luego usar la que se creó en dev y abrirla,
+ acá se ejecutará el server y cada vez que se guarde los cambios no habra necesidad de 
+ ejecutarlo de nuevo porque se hace automatico, solo se ejecuta la solucitud con http :3000 
+ para poder hacer la solicitud al server.*/
