269365bacc714799fc784b382b3232c9b5774e1a
Valeria <valeriamorales40@gmail.com>
2020-05-11 11:37:11 -0600

    Co-authored-by: Gabriel Abarca Aguilar <gababarca@hotmail.com>

--- VSPtr.cpp
+++ VSPtr.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 void GarbageCollector :: thread(){
     try{
             while(true){
@@ -25,25 +26,38 @@ void GarbageCollector :: saveAddress(int VSPtrCount, int* ptr){
     
 }
 
+
 void GarbageCollector :: generateID(){
 
     string ID = "VSPTR_" + to_string(VSPtrCount);
     GarbageCollector::IDs[VSPtrCount-1] = ID;
+
 
     for(int i=0; i< (sizeof(GarbageCollector::addess))/(sizeof(GarbageCollector::addess[0])) && GarbageCollector::addess[i] != 0; i++){
 
         cout << "ID (" << i << ") " << GarbageCollector::IDs[i] << endl;
     }
     
-    
+    cout << "_________________________________"<< endl;
+
 }
 
+string GarbageCollector::getID(int key){
+    return GarbageCollector::IDs[key];
+}
 
+int* GarbageCollector::getAdress(int key){
+    return GarbageCollector::addess[key];
+}
 
 int main(){
     //VSPtr<int> myPtr = VSPtr<int> ::New();
+ 
     VSPtr<int> ptr(new int());
+    *ptr = 20;
     VSPtr<int> ptr2(new int());
+    *ptr2 =30;
+    ptr = ptr2;
     return 0;
 
 }
--- VSPtr.h
+++ VSPtr.h
@@ -8,23 +8,24 @@ using namespace std;
 
 static int VSPtrCount;
 
+// ___________________________GARBAGE COLLECTOR_____________________________
+
 class GarbageCollector{
 
 public:
-    static GarbageCollector* getInstance();
-    void saveAddress(int VSPtrCount, int* ptr);
-    int *addess[10];
-    string IDs[10];
+    static GarbageCollector* getInstance();     //retorna el numero de instancias de un VSPtr
+    void saveAddress(int VSPtrCount, int* ptr);     // guardado del puntero
+    int *addess[10];        //lista de direcciones
+    string IDs[10];         //lista de ID
     void generateID();
+    string getID(int key);
+    int* getAdress(int key);
 
 private: 
     static GarbageCollector* instance;
     GarbageCollector();
     void thread();
     
-    
-
-
 };
 
 GarbageCollector* GarbageCollector:: instance = 0;
@@ -45,6 +46,7 @@ class VSPtr{
     T *ptr;
 
 public: 
+    int key;
 
     //constructor
     explicit VSPtr(T *p = NULL) {           //New()
@@ -52,6 +54,7 @@ public:
         GarbageCollector* g = GarbageCollector::getInstance();
         VSPtrCount++;
         cout << "VSPTR COUNT: " << VSPtrCount << endl;
+        key = VSPtrCount-1;
         g->saveAddress(VSPtrCount, ptr);
         g->generateID();
         
@@ -60,6 +63,8 @@ public:
     ~VSPtr(){
         delete(ptr);
     }
+
+    string ID;
 
     //Overloading deferencing operator 
     T & operator * (){
@@ -72,12 +77,22 @@ public:
         return ptr;
     }
 
-    VSPtr operator = (VSPtr const c1){              //ARREGLAR
-        if (typeid(VSPtr) == typeid(c1)){
-                cout << "Son de tipo VSPtr" << endl;
-        }
-    }
 
+    VSPtr operator = (VSPtr ptr){  
+      GarbageCollector* g = GarbageCollector::getInstance();
+
+      cout << "------ USING = OPERAND ------" << endl;
+      cout << "ACTUAL ID: " << g->getID(this->key) << endl;
+      cout << "ACTUAL ADDRESS: " << g->getAdress(this->key) << "\n" << endl;
+
+      g->IDs[this->key] = g->getID(ptr.key); //reemplaza el id del elemento izquierdo por una copia del id del elemento derecho
+      g->addess[this->key] = g->getAdress(ptr.key); //reemplaza el address del elemento izquierdo por una copia del address del elemento derecho
+
+      cout << "NEW ID: " << g->getID(this->key) << endl;
+      cout << "NEW ADDRESS: " << g->getAdress(this->key) << endl;
+
+
+    }
 
 };
 
