ba3dd5db4568cffdf6c70d43d1235e48b93c4552
Valeria <valeriamorales40@gmail.com>
2020-06-11 23:58:18 -0600

    server information verification changes

---
+++ VSPointer/cmake-build-debug/cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/VSPtr.cpp.o"
+  "libuntitled.a"
+  "libuntitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
